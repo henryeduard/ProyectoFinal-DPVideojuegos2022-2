310bbe074a67d5e6f5149beda252c035259a9d84
AngelCorrea <cgjangel@ciencias.unam.mx>
2022-04-21 19:49:17 -0500

    Movimiento del enemigo implementado Rotación sobre Y

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -10,41 +10,87 @@ public class MovimientoEnemigo : MonoBehaviour
     public float radioAtaqueX;
     public float radioAtaqueY;
 
+    public float velocidad;
+
     int direccionX;
     int direccionY;
 
     private float distAlObjetivoX;
     private float distAlObjetivoY;
+
+    private bool viendoDerecha = false;
+    
+
+
+    public GameObject golpe;
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         jugadores = GameObject.FindGameObjectsWithTag("Player");
         distAlObjetivoX = Mathf.Abs(jugadores[0].transform.position.x - transform.position.x);
         distAlObjetivoY = Mathf.Abs(jugadores[0].transform.position.y - transform.position.y);
-
+        rb = GetComponent<Rigidbody2D>();
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    private void FixedUpdate()
     {
 
+        distAlObjetivoX = Mathf.Abs(jugadores[0].transform.position.x - transform.position.x);
+        distAlObjetivoY = Mathf.Abs(jugadores[0].transform.position.y - transform.position.y);
+
         //Si no esta a radio de ataque, muevete e intenta igualar la posicion.
-        if(distAlObjetivoX > radioAtaqueX) 
-        { 
-            if (jugadores[0].transform.position.x > transform.position.x) direccionX = 1;
+        if (distAlObjetivoX > radioAtaqueX)
+        {
+            if (jugadores[0].transform.position.x > transform.position.x)  direccionX = 1;
             if (jugadores[0].transform.position.x <= transform.position.x) direccionX = -1;
         }
+        else 
+        {
+            direccionX = 0;
+        }
+
+
         if (distAlObjetivoY > radioAtaqueY)
         {
             if (jugadores[0].transform.position.y > transform.position.y) direccionY = 1;
             if (jugadores[0].transform.position.y <= transform.position.y) direccionY = -1;
+
+            
+        }
+        else 
+        {
+            direccionY = 0;
         }
 
-        if((distAlObjetivoY <= radioAtaqueY) && (distAlObjetivoX <= radioAtaqueX))
+        //Control de giro del enemigo
+        if(direccionX > 0 && !viendoDerecha)
+        {
+            girar();
+        }else if (direccionX < 0 && viendoDerecha)
+        {
+            girar();
+        }
+
+
+        //Esta a distancia de ataque
+        if ((distAlObjetivoY <= radioAtaqueY) && (distAlObjetivoX <= radioAtaqueX))
         {
             //ataca
+            
+            Debug.Log("Atacando");
         }
 
+        rb.velocity = new Vector2(direccionX*velocidad, direccionY*velocidad);
+    }
 
+    private void girar()
+    {
+        viendoDerecha = !viendoDerecha;
+        transform.eulerAngles = new Vector3(0,transform.eulerAngles.y + 180, 0);
     }
 }
