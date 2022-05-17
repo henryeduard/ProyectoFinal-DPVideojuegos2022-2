ca11e1865a220aab436e1683269c69797b65a8b5
AngelCorrea <cgjangel@ciencias.unam.mx>
2022-05-17 17:44:52 -0500

    Barra de vida implementada Falta configurar ataques

--- Assets/Scripts/Salud.cs
+++ Assets/Scripts/Salud.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class Salud : MonoBehaviour
 {
     public int vida;
+    [SerializeField] private int vidaMaxima;
 
+    [SerializeField] private BarraDeVida barraDeVida;
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        vida = vidaMaxima;
+        barraDeVida.IniciaBarraVida(vida);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (vida <= 0)
-            Destroy(gameObject);
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (vida <= 0)
+    //        Destroy(gameObject);
+    //}
 
     // Disminuye la vida.
     public void BajarVida(int daño) {
         vida = vida - daño;
+        barraDeVida.CambiaVidaActual(vida);
+        if (vida <= 0)
+            Destroy(gameObject);
     }
 }
