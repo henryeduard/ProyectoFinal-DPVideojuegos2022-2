fba10c9e7fae6ae5e1bfc334af57fd4714670802
Enrique Alcántara <enrique_eduaa@ciencias.unam.mx>
2022-05-23 01:04:42 -0500

    Script para que la cámara siga al jugador ya existe, pero tiene errores, queda pendiente.

--- Assets/Scripts/Camara.cs
+++ Assets/Scripts/Camara.cs
@@ -6,13 +6,30 @@ public class Camara : MonoBehaviour
 {
 
     // Jugador al que seguira la camara
-    public Transform jugador;
+    [SerializeField]
+    private GameObject jugador;
+
+    private Transform jugadorT;
+
+
+    /*private void Awake() 
+    {
+        jugador = GameObject.FindWithTag("Player");
+
+    }*/
     
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.position = new Vector3(jugador.position.x, jugador.position.y, this.transform.position.z);
+
+        if (jugador == null) {
+            jugador = GameObject.FindWithTag("Player");
+            jugadorT = jugador.transform;
+            Debug.Log("HOLA");
+        }
+
+        this.transform.position = new Vector3(jugadorT.position.x, jugadorT.position.y , this.transform.position.z);
 
     }
 }
